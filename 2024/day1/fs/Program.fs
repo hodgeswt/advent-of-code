811652c1f581fffbd36700d48e2ba9c3d95da50c
Hodges, Will <whodges9@gatech.edu>
2024-12-01 17:46:17 -0500

    fsharp part 1

---
+++ 2024/day1/fs/Program.fs
@@ -0,0 +1,33 @@
+﻿open System.IO
+
+module Day1 =
+    let Run path =
+        File.ReadLines path
+        |> Seq.map(fun x -> x.Split("   "))
+        |> Seq.map(fun x -> x |> Array.map int)
+        |> Seq.map(fun x -> (x.[0], x.[1]))
+        |> Seq.toArray
+        |> Array.unzip
+        |> fun (x, y) -> (x |> Array.sort, y |> Array.sort)
+        |> fun (x, y) -> Array.zip x y
+        |> Array.map(fun (x, y) -> abs(x - y))
+        |> Array.sum
+        |> printfn "%d"
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length = 0 then
+        // Run actual data
+        Day1.Run "../day1.input"
+        0
+    else
+        if argv.[0] <> "-t" then
+            printfn "Only accepted argument is -t"
+            1
+        else
+            // Run tests
+            Day1.Run  "../day1.test"
+            0
+
+
+
